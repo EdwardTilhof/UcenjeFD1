d0b2cdd0da1f1616a2862d1ff1778ed58b42c5f4
EdwardTilhof <edward.tilhof@gmail.com>
2026-01-27 18:49:46 +0100

    java scrip hello world

---
+++ 2_JS/vjezba01/scripta.js
@@ -0,0 +1,13 @@
+console.info('Hello world 5');
+console.table('{"name":"John", "age":30, "car":null}');
+
+console.log('O\'Neal'); // \ ALTGRB + Q
+console.log("O'Neal");
+
+console.log(`
+    Hello
+    world
+    6`); // ALTGRB + 7
+
+    document.getElementById('ispis').innerHTML='<a href="#">Poveznica</a>';
+    
