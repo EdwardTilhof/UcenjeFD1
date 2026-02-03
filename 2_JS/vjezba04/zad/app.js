32df5b7dc5ed5ef3b58d19ca318e5264e72a609a
EdwardTilhof <edward.tilhof@gmail.com>
2026-02-03 18:56:15 +0100

    zadatak 4 u vjezbi 4 odradjen

--- 2_JS/vjezba04/zad/app.js
+++ 2_JS/vjezba04/zad/app.js
@@ -1,6 +1,7 @@
-
 const rezultat = document.getElementById('rezultat');
+
 document.getElementById('izvedi').addEventListener('click', () => {
+  rezultat.innerHTML = '';
 
   const a = document.getElementById('a').value;
   const b = document.getElementById('b').value;
@@ -8,21 +9,129 @@ document.getElementById('izvedi').addEventListener('click', () => {
   const d = document.getElementById('d').value;
   const zadatak = document.getElementById('zadatak').value;
 
-  // Za svaki zadatak zahtijevamo određene ulaze
+  // -----------------------
+  // ZADATAK 1
+  // -----------------------
   if (zadatak === '1') {
-    console.log('1. zadatak');
-    console.log(a);
-    
-    
-    return; // short curcuiting
+    const x = Number(a);
+    const y = Number(b);
+
+    // Provjera da li su brojevi
+    if (isNaN(x)) {
+      rezultat.innerHTML = 'A nije broj';
+      return;
+    }
+
+    if (isNaN(y)) {
+      rezultat.innerHTML = 'B nije broj';
+      return;
+    }
+
+    // Usporedba
+    if (x > y) {
+      rezultat.innerHTML = x;
+    } else if (y > x) {
+      rezultat.innerHTML = y;
+    } else {
+      rezultat.innerHTML = 'A i B su isti';
+    }
+
+    return; // short-circuiting
   }
 
-  // Ovdje će doći drugi zadatak
+  // -----------------------
+  // ZADATAK 2
+  // -----------------------
+  if (zadatak === '2') {
+    const x = Number(a);
+    const y = Number(b);
+    const z = Number(c);
 
-  
-  // ovo će se ispisati ako u HTML dodatke option za zadatak a ovdje ga ne obradite
+    // Provjera brojeva
+    if (isNaN(x)) {
+      rezultat.innerHTML = 'A nije broj';
+      return;
+    }
+    if (isNaN(y)) {
+      rezultat.innerHTML = 'B nije broj';
+      return;
+    }
+    if (isNaN(z)) {
+      rezultat.innerHTML = 'C nije broj';
+      return;
+    }
+
+    // Najmanji broj
+    if (x <= y && x <= z) {
+      rezultat.innerHTML = x;
+    } else if (y <= x && y <= z) {
+      rezultat.innerHTML = y;
+    } else {
+      rezultat.innerHTML = z;
+    }
+
+    return;
+  }
+
+  // zadatak 3
+
+  if (zadatak === '3') {
+    const x = Number(a);
+    const y = Number(b);
+    const z = Number(c);
+    const w = Number(d);
+
+    if (isNaN(x)) {
+      rezultat.innerHTML = 'A nije broj';
+      return;
+    }
+    if (isNaN(y)) {
+      rezultat.innerHTML = 'B nije broj'
+      return;
+    }
+    if (isNaN(z)) {
+      rezultat.innerHTML = 'C nije broj'
+      return;
+    }
+    if (isNaN(w)) {
+      rezultat.innerHTML = 'D nije broj'
+      return;
+    }
+
+    rezultat.innerHTML = x + y + z + w;
+
+    //    rezultat.innerHTML = a + b + c + d ( ovdje spaja stringove ) 
+
+    return;
+  }
+
+  //-------------------
+  //  4 Zadatak
+  //-------------------
+
+  // Korisnik unosi godinu rodjenja u polje A
+  // Izracunaj njegovu dob u odnosu na tekucu godinu i ispisi dali je punoljetan
+
+  if (zadatak === '4') {
+    const x = Number(a)
+
+    if (isNaN(x)) {
+      rezultat.innerHTML = 'Niste unjeli ispravan broj ili znak. molimo unesite broj'
+      return;
+    }
+    if (x < 18) {
+      rezultat.innerHTML = 'Maloljetni ste'
+      return;
+    } else if (x >= 18) {
+      rezultat.innerHTML = 'Punoljetni ste'
+      return;
+    }
+  }
+
+
+
+  // -----------------------
+  // Nepoznati zadatak
+  // -----------------------
   rezultat.innerHTML = `Nepoznati zadatak ${zadatak}`;
 });
-
-
-
